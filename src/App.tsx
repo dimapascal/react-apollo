7c81ec70b49f895428803012359b7645aff5e157
dimapascal <dimapascal@gmail.com>
2020-12-02 15:57:05 +0200

    Implement apollo in react app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { DefaultRouter } from "./Router";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./Services/apolloClient";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <DefaultRouter />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <DefaultRouter />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
